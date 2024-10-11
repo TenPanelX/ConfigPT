5144500f3173a9e60d6f9fa0cc98d68743ae64ac
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2024-10-11 07:03:31 +0700

    Update X2.lua

--- ConfigPerPC/X2.lua
+++ ConfigPerPC/X2.lua
@@ -1,1 +1,7 @@
-
+shared.Configs = {
+    Enabled = true,
+    RedeemCode = true,
+    GithubUrl = "https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/Summon.lua",
+    JobID = "",
+    TradingMode = false,
+};

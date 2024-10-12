49b3f2f878020cf64854fdaa491d6780009a3957
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2024-10-12 23:54:00 +0700

    Update X1.lua

--- ConfigPerPC/X1.lua
+++ ConfigPerPC/X1.lua
@@ -1,7 +1,14 @@
 shared.Configs = {
     Enabled = true,
     RedeemCode = true,
-    GithubUrl = "https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/Summon.lua",
-    JobID = "",
+    Speed = 2,  
+    GithubUrl = 'https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/Summon.lua',
+    GithubConfigUrl = 'https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/ConfigPerPC/X1.lua',
+    JobID = '',
     TradingMode = false,
+    AutoTrading = {
+        ['Enabled'] = false,
+        ['Main User'] = '',
+        ['JobID'] = '',
+    },
 };

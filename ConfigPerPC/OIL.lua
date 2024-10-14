0b9f3e7eba78b70fd0277a5852a208a427000c68
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2024-10-14 21:14:43 +0700

    Create OIL.lua

---
+++ ConfigPerPC/OIL.lua
@@ -0,0 +1,14 @@
+shared.Configs = {
+    Enabled = true,
+    RedeemCode = true,
+    Speed = 2,  
+    GithubUrl = 'https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/SummonOIL.lua',
+    GithubConfigUrl = 'https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/ConfigPerPC/OIL.lua',
+    JobID = '',
+    TradingMode = false,
+    AutoTrading = {
+        ['Enabled'] = false,
+        ['Main User'] = 'Amparex59489',
+        ['JobID'] = '',
+    },
+};

cb4e0639b7475fecbbff347cd567d92ae89bc78b
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2024-10-14 14:58:10 +0700

    Update Vote.lua

--- Vote.lua
+++ Vote.lua
@@ -8,7 +8,7 @@
             if PlayerGui.MainUI.Frames.Voting.Visible then 
                 if LocalPlayer:DistanceFromCharacter(CFrameCheck.Position) >= 500 then 
                     local args = {
-                        [1] = "Medium"
+                        [1] = "Hard"
                     }
                     
                     game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.7.0").knit.Services.VotingService.RF.Vote:InvokeServer(unpack(args))

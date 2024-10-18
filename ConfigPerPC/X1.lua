0176ab28c0b338bb7641290362454d914c941bed
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2024-10-19 01:10:51 +0700

    Update X1.lua

--- ConfigPerPC/X1.lua
+++ ConfigPerPC/X1.lua
@@ -7,7 +7,7 @@ shared.Configs = {
     JobID = '',
     TradingMode = false,
     AutoTrading = {
-        ['Enabled'] = true,
+        ['Enabled'] = false,
         ['Main User'] = 'KobeLensKid', -- Username Host
         ['JobID'] = '', -- ต้องแก้ JobID ทุกครั้งก่อน Trade
     },

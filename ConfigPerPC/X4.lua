b496555682d7dcef199201b3dc98d5dc3ce6836e
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2024-10-19 00:13:52 +0700

    Update X4.lua

--- ConfigPerPC/X4.lua
+++ ConfigPerPC/X4.lua
@@ -9,6 +9,6 @@ shared.Configs = {
     AutoTrading = {
         ['Enabled'] = true,
         ['Main User'] = 'GymPoodleDay',
-        ['JobID'] = 'f26ec5d3-f649-436f-94e5-0a376da97bb0',
+        ['JobID'] = '',
     },
 };

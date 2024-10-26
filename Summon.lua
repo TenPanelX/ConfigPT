802898d7bd6f6280e94aa4a8eded220c112dc6f7
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2024-10-26 10:01:17 +0700

    Update Summon.lua

--- Summon.lua
+++ Summon.lua
@@ -1,5 +1,5 @@
 return {
-  ['Enabled'] = false, -- เปิดเพื่อให้กลับ Lobby ละรอ 5-6 นาที
+  ['Enabled'] = true, -- เปิดเพื่อให้กลับ Lobby ละรอ 5-6 นาที
   ['Roll'] = false, -- true เพื่อสุ่มตัว
   ['Event'] = false,
 };

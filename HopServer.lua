10dcab223e2c843812e48685de159bcc627332c0
TenPanelX <85727022+TenPanelX@users.noreply.github.com>
2025-12-02 14:25:32 +0700

    Update and rename HopServer.lua to Hoppa.lua

--- HopServer.lua
+++
@@ -1,8 +0,0 @@
-_G.Parties = {
-    PartyName = "SusFirst",
-    License = "c19fc475-da41-492f-995a-b47f932e5ca6",
-    Capacity = 8,
-    Cooldown = nil,
-}
-
-loadstring(game:HttpGet("https://deity.alphes.net/Files/parties.lua"))()
---
+++ Hoppa.lua
@@ -0,0 +1,58 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+task.spawn(function()
+    task.wait(10)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/AddV2.lua"))()
+end)
+
+task.spawn(function()
+    task.wait(10)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TenPanelX/ConfigPT/refs/heads/main/BlackScreen.lua"))()
+end)
+
+getgenv().HermanosDevSetting = {
+    Farming = {
+        Job = "Shelf Stocker", -- Shelf Stocker, Cook, Janitor, Swiper, Fishing, Farming
+
+        -- Cook
+        Skillet = "Smart Select",
+        BuySkillet = false,
+
+        -- Janitor
+        PaddleMode = "Nearest", -- Smart, Nearest
+        Mop = "Smart Select",
+        BuyMop = false,
+
+        -- ATM Hacking
+        HackTools = "Smart Select",
+        HackToolsQuantity = 5,
+        
+        -- Fishing
+        Rod = "Smart Select",
+        Bait = "Smart Select",
+        BaitQuantity = 10,
+        FishAmount = 10,
+        AutoSellFish = false,
+
+        -- Vehicle
+        VehicleType = "Bike", -- Bike, Car
+        VehicleSpeed = 52,
+
+        -- Auto Farm
+        AutoFarm = true,
+        AfkChecker = true,
+
+        -- Deposit
+        CashDeposit = 50,
+        AutoDeposit = true
+    },
+
+    General = {
+        HideName = true,
+        AntiRagdoll = true,
+        AntiKill = true,
+        AutoRespawn = true,
+    },
+}
